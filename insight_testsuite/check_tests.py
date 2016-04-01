e361b25a0f74f9acef1cb66460104bef2826357c
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2016-04-01 10:04:29 +0300

    implement unit test with exactly the same hashtags

--- insight_testsuite/check_tests.py
+++ insight_testsuite/check_tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 dir1= "./insight_testsuite/tests/test-2-tweets-nohashtag/tweet_output/output.txt"
+dir2="./insight_testsuite/tests/test-2-tweets-all-equal/tweet_output/output.txt"
 if dir1==sys.argv[1]:
 	error=0;
 	with open (sys.argv[1], 'r') as f :
@@ -10,6 +11,16 @@ if dir1==sys.argv[1]:
 			else:
 				error=1
 				print("test failed!")
+if dir2==sys.argv[1]:
+	error=0;
+	with open (sys.argv[1], 'r') as f :
+		for i in f:
+			if str(i.rstrip())=="1.00":
+				print("test passed!")
+			else:
+				error=1
+				print("test failed!")
+
 
 
 
--- insight_testsuite/myrun_tests.sh
+++ insight_testsuite/myrun_tests.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 cd ..
 
-#run first test
+#test for two tweets with no hashtag:
 python ./src/average_degree.py ./insight_testsuite/tests/test-2-tweets-nohashtag/tweet_input/tweets.txt  ./insight_testsuite/tests/test-2-tweets-nohashtag/tweet_output/output.txt
-
 #should get 0.00 (first tweet), 0.00 (first 02 tweets)
 python ./insight_testsuite/check_tests.py ./insight_testsuite/tests/test-2-tweets-nohashtag/tweet_output/output.txt
 
 
+#test for two tweets with exactly the same tag:
+python ./src/average_degree.py ./insight_testsuite/tests/test-2-tweets-all-equal/tweet_input/tweets.txt  ./insight_testsuite/tests/test-2-tweets-all-equal/tweet_output/output.txt
+#should get 1.00 (first tweet), 1.00 (first 02 tweets)
+python ./insight_testsuite/check_tests.py ./insight_testsuite/tests/test-2-tweets-all-equal/tweet_output/output.txt
